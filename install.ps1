eb74ee63f7c436aed4e385fda59549c475554feb
grafst <grafst@users.noreply.github.com>
2023-07-04 12:06:03 +0200

    refresh environment after choco install

--- install.ps1
+++ install.ps1
@@ -4,6 +4,8 @@ Function Install-Chocolatey {
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
     choco feature enable -n=allowGlobalConfirmation
+    Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+    RefreshEnv.cmd
 }
 function New-TemporaryDirectory {
     $parent = [System.IO.Path]::GetTempPath()
