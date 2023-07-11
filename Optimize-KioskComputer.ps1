72647d28697587dfb4039a453421a4ec75399211
grafst <grafst@users.noreply.github.com>
2023-07-11 15:13:08 +0200

    global variables now working?

--- Optimize-KioskComputer.ps1
+++ Optimize-KioskComputer.ps1
@@ -1,11 +1,16 @@
 #requires -RunAsAdministrator
 
+
+$global:Computername = Read-Host -Prompt "Computername: "
+#$DeepFreezeKey = Read-Host -Prompt "DeepFreeze Key: "
+$global:DeepFreezePassword = Read-Host "DeepFreeze Password" -AsSecureString
+
+
 . .\functions.ps1
 . .\tasks.ps1
 
-$Computername = Read-Host -Prompt "Computername: "
-$DeepFreezeKey = Read-Host -Prompt "DeepFreeze Key: "
-$global:DeepFreezePassword = Read-Host "DeepFreeze Password" -AsSecureString
+
+
 $Tasks.GetEnumerator() | ForEach-Object {
     Write-Host $_.Key
     Invoke-Command $_.Value
