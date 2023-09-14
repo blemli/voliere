2460dc2c2c3ecca8eff80205508d76146769f5ca
grafst <grafst@users.noreply.github.com>
2023-09-14 12:09:12 +0200

    wip

--- tasks.ps1
+++ tasks.ps1
@@ -1,6 +1,6 @@
 . .\functions.ps1
 
-$Tasks ={"Vogelsang-Internet"=[ordered]@{
+$Tasks =@{"Vogelsang-Internet"=[ordered]@{
     "Computer umbenennen"={Rename-Computer -NewName $global:ComputerName}
     "Dateiendungen einblenden"={ShowKnownExtensions};
     "3D Objekte verstecken"={Hide3DObjectsFromExplorer};
