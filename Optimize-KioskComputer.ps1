5e3d9075a59862b9ad7e8e4dc29a90edee9e03e3
grafst <grafst@users.noreply.github.com>
2023-09-15 09:19:25 +0200

    wip

--- Optimize-KioskComputer.ps1
+++ Optimize-KioskComputer.ps1
@@ -9,11 +9,7 @@ $Template = Read-Host -Prompt "Template"
 $global:Computername = Read-Host -Prompt "Computername"
 $encryptedPW=Read-Host -AsSecureString -Prompt "DeepFreeze Password" 
 $global:DeepFreezePassword = get-cleartext($encryptedPW)
-write-host $global:DeepFreezePassword
 $global:username = Read-Host -Prompt "Username"
-
-
-
 
 
 $Tasks[$Template].GetEnumerator() | ForEach-Object {
